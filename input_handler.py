44eac08d2a1e26a484ee0248ad3c4630acecca9b
hirasawa <hirasawa@mabc.ac.jp>
2025-10-22 15:49:38 +0900

    括弧とべき乗の処理を再実装

--- input_handler.py
+++ input_handler.py
@@ -2,12 +2,18 @@ from utils import format_result
 from utils import ALLOWED_OPERATORS
 import re
 
+
 class CalculatorState:
     def __init__(self):
         self.just_evaluated = False
 
+
 def handle_input(text: str, screen, state):
     current = screen.get()
+
+    # 19文字以上だったらクリア以外入力できない
+    if len(current) >=19 and text not in ("c","C"):
+        return
 
     # クリア
     if text.lower() == "c":
@@ -17,12 +23,16 @@ def handle_input(text: str, screen, state):
     
     # イコール
     if text == "=":
-        try:
-            result = format_result(current)
-            screen.set(result)
-        except Exception as e:
-            screen.set("エラー")
-            print(e)
+        if current[-1] in ALLOWED_OPERATORS:
+            return
+        else:
+            try:
+                result = format_result(current)
+                screen.set(result)
+                state.just_evaluated = True
+            except Exception as e:
+                screen.set("エラー")
+                print(e)
         state.just_evaluated = True
         return
     
@@ -59,34 +69,63 @@ def handle_input(text: str, screen, state):
         screen.set(current[:last_op_index + 1] + new_text)
         return
         
-    
     # 数字
     if text.isdigit():
-        if current and current[-1] in "+-*/%.":
-            screen.set(current + text)
-        else:        
+        if state.just_evaluated:
             screen.set(text)
+        else:
+            if current == "0" or (len(current) >= 2 and current[-1] == "0" and current[-2] in "+-/*"):
+                screen.set(current[:-1] + text)
+            elif current and current[-1] in ")":
+                screen.set(current + "*" + text)
+            else:
+                screen.set(current + text)
         state.just_evaluated = False
         return
     
-    # 演算子
-    if text in ALLOWED_OPERATORS:
+    # 演算子(べき乗とプラスマイナスキーの処理も)
+    if text in ALLOWED_OPERATORS or text == "*" or text == "-":
         if len(current) == 0:
+            return
+        if current[-2:] == "**":
+            return
+        if current[-2:] == "--":
+            return
+        if current[-1] == "*" and text == "*":
+            screen.set(current + text)
+            return
+        if current[-1] == "-" and text == "-":
+            screen.set(current + text)
             return
         if current[-1] in ALLOWED_OPERATORS:
             screen.set(current[:-1] + text)
         else:
             screen.set(current + text)
-        state.just_evaluated = False
         return
     
     # 括弧
-    if text in "()":
-        screen.set(current + text)
+    if text == "(":
+        if len(current) == 0:
+            screen.set(text)
+        elif current[-1] in "1234567890":
+            screen.set(current + "*" + text)
+        else:    
+            screen.set(current + text)
         state.just_evaluated = False
         return
     
-    # その他
+    if text == ")":
+        if current.count("(") > current.count(")"):
+            if current[-1] in "(":
+                return
+            else:
+                screen.set(current + text)
+        else:
+            return
+        state.just_evaluated = False
+        return
+
+    # 計算後の数字の追記を防ぐ処理
     if state.just_evaluated:
         if text in ALLOWED_OPERATORS:
             screen.set(current + text)
